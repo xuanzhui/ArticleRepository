aaeacb0fb90aa2dda29a4b05d9ba3829c2d4fe17
xuanzhui <daiwei1215@126.com>
2015-04-05 22:03:54 +0800

    .

--- tangpoetry/extractpoemcont.py
+++ tangpoetry/extractpoemcont.py
@@ -13,25 +13,40 @@ import traceback
 class ExtractPoemCont:
 
     def extractPoem(self, conturl):
-        poem='NULL'
+        poem=None
         try:
+            #set timeout 8 seconds in case process blocks within too much time
+            #use replace instead of ignore as the second parameter of decode and use GB18030 instead of gbk to support more characters
+            urlcont=urllib.request.urlopen(conturl, timeout=8).read().decode('GB18030','replace')
 
-            urlcont=urllib.request.urlopen(conturl).read().decode('gbk', 'ignore')
-            pattern=r'<font face="幼圆" style="font-size: 16pt" color="#FFFFBF">(.*?)</font>'
-            patternobj=re.compile(pattern)
-            poems=patternobj.findall(urlcont)
-
-            poem=poems[0].replace('&nbsp;','')
-            poem=poem.replace('<br>','')
         except Exception:
+            urlcont=None
             print('Exception!!!!')
             print(conturl)
             print(traceback.format_exc())
+
+        if urlcont:
+            #pattern=r'<font face="幼圆" style="font-size: 16pt" color="#FFFFBF">(.*?)</font>'
+            pattern=r'<font face="幼圆" style="font-size: \d*pt" color="#FFFFBF">(.*?)</font>'
+            patternobj=re.compile(pattern)
+            poem=patternobj.search(urlcont)
+
+            if poem:
+                poem=poem.group(1)
+                poem=poem.replace('&nbsp;','')
+                poem=poem.replace('<br>','')
+            else:
+                poem=None
+                print('retrieve content failed!!!')
 
         return poem
 
     def store2db(self, conn, curl, aid):
         cont=self.extractPoem(curl)
+
+        if not cont:
+            return
+
         cur = conn.cursor()
         cur.execute("update articles set content = %s where id = %s",
             (cont, aid))
@@ -92,6 +107,22 @@ class ExtractPoemCont:
         pool.close()
         pool.join()
 
+    #this is only for the ones whose contents are still not filled
+    def wipeleft(self, password, showprocess=True):
+        conn = psycopg2.connect(database="poems", user="poet", password=password)
+
+        cur=conn.cursor()
+        cur.execute("select id, additional_info1 from articles where content='NULL'")
+        recds = cur.fetchall()
+        cur.close()
+
+        for aid, link in recds:
+            if showprocess:
+                print('dealing with --> ', link)
+            self.store2db(conn, link, aid)
+
+        conn.close()
+
 
 if __name__=='__main__':
     import getpass
@@ -100,6 +131,7 @@ if __name__=='__main__':
 
     print('processing...')
 
-    ExtractPoemCont().batch_store2db(password, showprocess=True)
+    #ExtractPoemCont().batch_store2db(password, showprocess=True)
+    ExtractPoemCont().wipeleft(password)
 
     print('done!')
